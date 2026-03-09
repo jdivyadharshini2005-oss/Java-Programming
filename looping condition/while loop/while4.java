080d2e624582f29959b713b0dd4abf967457bb89
jdivyadharshini2005-oss <jdivyadharshini2005@gmail.com>
2026-03-09 21:21:32 +0530

    Add files via upload

---
+++ looping condition/while loop/while4.java
@@ -0,0 +1,16 @@
+package loop;
+import java.util.*;
+public class while4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int i=1;
+        int fact=1;
+        while(i<=n){
+            fact=fact*i;
+            i++;
+        }
+        System.out.println("Factorial of " +n +" = " +fact);
+    }
+    
+}
